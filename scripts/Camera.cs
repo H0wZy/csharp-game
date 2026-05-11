216f2372b117d93ac3793c1f6e52807c6c03c6dd
H0wZy <h0wzymarcos@gmail.com>
2026-05-11 13:16:24 -0300

    feat: custom camera

---
+++ scripts/Camera.cs
@@ -0,0 +1,33 @@
+using Godot;
+
+public partial class Camera : Camera2D
+{
+	private Node2D _target;
+	[Export] private float _followSpeed = 5f;
+
+	public override void _Ready()
+	{
+		GetTarget();
+	}
+
+	public override void _Process(double delta)
+	{
+		if (_target == null)
+			return;
+
+		GlobalPosition = GlobalPosition.Lerp(_target.GlobalPosition, _followSpeed * (float)delta);
+	}
+
+	private void GetTarget()
+	{
+		var nodes = GetTree().GetNodesInGroup("player_group");
+
+		if (nodes.Count <= 0)
+		{
+			GD.PushError("Player not found");
+			return;
+		}
+
+		_target = nodes[0] as Node2D;
+	}
+}
